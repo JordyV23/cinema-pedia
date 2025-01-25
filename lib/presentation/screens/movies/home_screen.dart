04dd0a609f9d6b69ea9c48fad162362cea003e7f
JordyV23 <jordyvargas0123@gmail.com>
2025-01-24 22:29:51 -0600

    Listado de peliculas
     Se muestra listado de peliculas en el front

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,5 +1,7 @@
 // import 'package:cinema_pedia/config/constants/environment.dart';
+import 'package:cinema_pedia/presentation/providers/movies/movies_providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
@@ -8,8 +10,37 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Placeholder()
-    );
+    return Scaffold(body: _HomeView());
+  }
+}
+
+class _HomeView extends ConsumerStatefulWidget {
+  const _HomeView();
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<_HomeView> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final nowPlayingMoviews = ref.watch(nowPlayingMoviesProvider);
+
+    if (nowPlayingMoviews.length == 0) return const CircularProgressIndicator();
+
+    return ListView.builder(
+        itemCount: nowPlayingMoviews.length,
+        itemBuilder: (context, index) {
+          final movie = nowPlayingMoviews[index];
+          return ListTile(
+            title: Text(movie.title),
+          );
+        });
   }
 }
